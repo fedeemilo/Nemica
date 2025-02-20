3dd6b2377c3e19c6c29fa2a1f9344ab6818eb825
Federico Milone <federico.milone@claro.com.ar>
2025-02-20 13:01:15 -0300

    Correct vite config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Nemica/',
-  plugins: [react(), tailwindcss()],
-  build:{
-   rollupOptions:{
-    output:{
-      inlineDynamicImports: true,
+    base: "./",
+    plugins: [react(), tailwindcss()],
+    build: {
+        rollupOptions: {
+            output: {
+                inlineDynamicImports: true,
+            },
+        },
     },
-   },
-  },
 });
-
